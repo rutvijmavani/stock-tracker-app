b745d810a3d145884f75edd8f7e57cb3a5c510b0
rutvijmavani <rutvijmavani@gmail.com>
2025-11-17 19:51:35 -0500

    reverting the commit

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -1,6 +1,6 @@
 import { email } from 'better-auth'
 import nodemailer from 'nodemailer'
-import { NEWS_SUMMARY_EMAIL_TEMPLATE, WELCOME_EMAIL_TEMPLATE } from './templates'
+import { WELCOME_EMAIL_TEMPLATE } from './templates'
 
 export const transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -25,21 +25,3 @@ export const sendWelcomeEmail = async ({ email , name , intro }: WelcomeEmailDat
 
     await transporter.sendMail(mailOptions)
 }
-
-export const sendNewsSummaryEmail = async (
-    { email, date, newsContent }: { email: string; date: string; newsContent: string }
-): Promise<void> => {
-    const htmlTemplate = NEWS_SUMMARY_EMAIL_TEMPLATE
-        .replace('{{date}}', date)
-        .replace('{{newsContent}}', newsContent);
-
-    const mailOptions = {
-        from: `"Signalist News" <signalist@jsmastery.pro>`,
-        to: email,
-        subject: `📈 Market News Summary Today - ${date}`,
-        text: `Today's market news summary from Signalist`,
-        html: htmlTemplate,
-    };
-
-    await transporter.sendMail(mailOptions);
-};
